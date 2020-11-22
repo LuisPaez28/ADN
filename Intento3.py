0e7b0995353f028d231adba2af36f58715d72ee6
LuisPaez28 <36243459+LuisPaez28@users.noreply.github.com>
2020-11-22 10:57:26 -0700

    Funcional

--- Intento3.py
+++ Intento3.py
@@ -26,6 +26,7 @@ def colorin(nucleoti):
         return 'yellow'
 inicio = 10
 inicio2 = 10
+inicioBien = 10
 #Simulo el ADN
 for x in range(100):
 #Creo el 5' 
@@ -87,10 +88,10 @@ for x in range(100):
         fosf.left(90)
         fosf.pendown()
         fosf.forward(inicio2+2)
-        #TODO falta hacer los otros escalones.
 
         nucleo =turtle.Turtle()
         nucleo.hideturtle()
+        nucleo.speed(10)
         nucleo.pensize(4)
         nucleo.left(90)
         nucleo.pu()
@@ -101,5 +102,77 @@ for x in range(100):
         nucleo.pencolor(usarColor)
         nucleo.forward(30)
         inicio2+=10
+    #Hacemos revision en el primer nucleotido
+    if n1==t:
+        n5=a
+        print(n5)
+    if n1==g:
+        n5=c
+        print(n5)
+    if n1==a:
+        n5=t
+        print(n5)
+    if n1==c:
+        n5=g
+        print(n5)
+    #Hacemos revision en el segundo nucleotido
+    if n2==t:
+        n6=a
+    if n2==g:
+        n6=c
+    if n2==a:
+        n6=t
+    if n2==c:
+        n6=g
+    #Hacemos revision en el tercer nucleotido
+    if n3==t:
+        n7=a
+    if n3==g:
+        n7=c
+    if n3==a:
+        n7=t
+    if n3==c:
+        n7=g
+    #Hacemos la revision en el cuarto nucleotido
+    if n4==t:
+        n8=a
+    if n4==g:
+        n8=c
+    if n4==a:
+        n8=t
+    if n4==c:
+        n8=g
+    tres=[n5,n6,n7,n8]
+    #Mostrar resultados
+    for i in range(4):
+        seleccionado = tres[i]
+        usarColor = colorin(seleccionado)
+
+        fosf = turtle.Turtle()
+        fosf.hideturtle()
+        fosf.color('black')
+        fosf.pensize(8)
+        fosf.penup()
+        fosf.forward(60)
+        fosf.left(90)
+        fosf.pendown()
+        fosf.forward(inicioBien+2)
+
+        nucleo =turtle.Turtle()
+        nucleo.hideturtle()
+        nucleo.speed(10)
+        nucleo.pensize(4)
+        nucleo.left(90)
+        nucleo.pu()
+        nucleo.forward(inicioBien)
+        nucleo.right(90)
+        nucleo.forward(30)
+        nucleo.pendown()
+        nucleo.pencolor(usarColor)
+        nucleo.forward(30)
+        inicioBien+=10
+        #Vamos a hacer la revisión, corrección y dibujado
+
+            
 
 #    inicio+=10
